b11de4e4a583b891e03310080db342bc19c9d6f9
Nick Baynham <nickbaynham@gmail.com>
2025-04-09 12:27:28 -0700

    added a click on link to the script

--- src/playright_python/app.py
+++ src/playright_python/app.py
@@ -4,13 +4,18 @@ print("Playwright Automation Framework")
 with sync_playwright() as playwright:
     print("Playwright Started.")
     # Launch a Browser
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    #browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    browser = playwright.chromium.launch()
     # Create a new Page
     page = browser.new_page()
     # Visit the Playwright Website
     page.goto("https://playwright.dev")
     # Visit another Playwright Website Page
     page.goto("https://playwright.dev/python")
+    # Locate a link element with 'Docs' as the name
+    docs_link = page.get_by_role("link", name="Docs")
+    # Click on the Docs link
+    docs_link.click()
     # Close the Browser
     browser.close()
 print("Playwright Completed.")
